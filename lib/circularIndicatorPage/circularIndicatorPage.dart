b171515a354e141a42223ba594b94d5fa07f9735
itssharma01 <mihirsharmajsr4@gmail.com>
2023-08-11 18:10:04 +0530

    circular Indicator page

---
+++ lib/circularIndicatorPage/circularIndicatorPage.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:pgvala_user/user/bookingPage.dart';
+
+
+
+class circularProgressIndicator extends StatefulWidget {
+  const circularProgressIndicator({super.key});
+
+  @override
+  State<circularProgressIndicator> createState() => _circularProgressIndicatorState();
+}
+
+class _circularProgressIndicatorState extends State<circularProgressIndicator> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(Duration(seconds: 3),(){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>bookingPage()));
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
